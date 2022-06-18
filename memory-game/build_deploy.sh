17fb2f0e5d8ead457a0b8e730a7f57fe28de1fc9
turbhekarprachi <turbhekarprachi@gmail.com>
2022-06-18 16:55:46 +0530

    Latest changes with dist directory

---
+++ memory-game/build_deploy.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+echo "Build started"
+npm run build
+
+echo "Deploy started"
+npm run deploy-S3
--- memory-game/vite.config.js
+++ memory-game/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const ASSET_URL = process.env.ASSET_URL || "";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
   },
+  base: `${ASSET_URL}/dist/`,
 })
